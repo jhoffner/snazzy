33659b3aae673cffe6e2edf2863e89095eedf8d7
jake hoffner <jakehoffner@Jakes-Macbook-Pro.local>
2012-04-09 17:50:42 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snazzy::Application.config.secret_token = '237bcfb5adc8b43a040693dfa508dbc31e22660be17731d560d0217db5bf62450e9e105282697e0bd02228a41dc05bb044355d291a9fa8c3d0e2b8dba329620d'
